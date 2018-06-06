700ad3779bab3702ae09c4c5cfd2c792dcdab8f6
tomohiro.sai <tomohiro.sai@rakuten.com>
2018-06-06 06:33:45 +0000

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     @else
         <div class="center jumbotron">
             <div class="text-center">
-                <h1>Welcome to the tasks</h1>
+                <h1>Welcome to the Tasklist</h1>
                 {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
