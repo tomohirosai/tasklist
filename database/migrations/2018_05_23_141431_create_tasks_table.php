d530c1c594320b2876f608cbb48b8d076732d492
tomohiro.sai <tomohiro.sai@rakuten.com>
2018-06-06 06:54:26 +0000

    aa

--- database/migrations/2018_05_23_141431_create_tasks_table.php
+++ database/migrations/2018_05_23_141431_create_tasks_table.php
@@ -14,11 +14,11 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->increments('id')
+            $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
             $table->string('content');
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users')
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
